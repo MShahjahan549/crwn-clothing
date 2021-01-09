2e38b68fdc23914957038fbf1928641feacac408
Mohammed Shahjahan <mohammedshahjahan@Mohammeds-MacBook-Pro.local>
2021-01-10 01:39:17 +0530

    created project with react app and added filess

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.scss';
+import Homepage from './homepage.component';
+
+function App() {
+  return (
+    <div >
+<Homepage/>
+
+    </div>
+  );
+}
+
+export default App;
